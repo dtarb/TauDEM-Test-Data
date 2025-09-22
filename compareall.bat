f5d55520028aba946ca3ee5c3fd332d5a603be04
pkdash <pabitra.dash@usu.edu>
2025-09-22 17:41:02 -0400

    Fix typo in compareall.bat for TestRunResult directory and update item list for CatchHydroGeo

--- compareall.bat
+++ compareall.bat
@@ -2,10 +2,9 @@ Rem Script to compare results for all folders for a specific test run
 Rem Usage: compareall.bat <test_run_folder>
 
 :: Define the list
-set items=Base fts Geographic gridtypes sinmapsi AreaD8_data AreaDinf Gridnet peukerDouglas streamnet_data D8flowextreme DinfConcLimAccum DinfTransLimAcc MovedOutletstoStream_data GageWatershed ConnectDown NoEPSG MoveOutlets2 MoveOutlets3 gwunittest editraster catchoutlets FlowdirCond RetLimFlow CatchHydroGeoTest GDAL_unset_nodata
+set items=Base fts Geographic gridtypes sinmapsi AreaD8_data AreaDinf Gridnet peukerDouglas streamnet_data D8flowextreme DinfConcLimAccum DinfTransLimAcc MovedOutletstoStream_data GageWatershed ConnectDown NoEPSG MoveOutlets2 MoveOutlets3 gwunittest editraster catchoutlets FlowdirCond RetLimFlow CatchHydroGeo Inundepth GDAL_unset_nodata
 
 :: Loop over the list
 for %%i in (%items%) do (
-    python compare_results.py ReferenceResult\%%i TestRunResults\%~1\%%i
+    python compare_results.py ReferenceResult\%%i TestRunResult\%~1\%%i
 )
-
