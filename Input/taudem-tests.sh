6aefc165e47d84fc2114b4de0d2ce20a6bb4c944
pkdash <pabitra.dash@usu.edu>
2025-12-27 23:04:31 -0500

    fix: simplify test cases in taudem-tests.sh by removing duplicates and adjusting command syntax

--- Input/taudem-tests.sh
+++ Input/taudem-tests.sh
@@ -14,9 +14,14 @@ if [ ! -d "$TAUDEM_PATH" ]; then
 fi
 
 # Basic grid analysis
-@test "run mpiexec -np 3 $TAUDEM_PATH/pitremove logan.tif -fel loganfel.tif" {
+@test "run mpiexec -np 3 $TAUDEM_PATH/pitremove logan.tif" {
 cd Base
 run mpiexec -np 3  $TAUDEM_PATH/pitremove logan.tif
+assert_success
+}
+@test "run mpiexec -np 3 $TAUDEM_PATH/pitremove -z logan.tif -fel loganfel.bil" {
+cd Base
+run mpiexec -np 3  $TAUDEM_PATH/pitremove -z logan.tif -fel loganfel.bil
 assert_success
 }
 @test "run mpiexec -np 3 $TAUDEM_PATH/pitremove -z logan.tif -fel loganfel4.tiff -4way" {
@@ -839,12 +844,6 @@ cd gridtypes
 run mpiexec -np 3  $TAUDEM_PATH/pitremove -z logan.tif -fel loganfel1.bin
 assert_success
 }
-# THIS IS A DUPLICATE OF THE TEST ABOVE (line 824)
-# @test "run mpiexec -np 3  $TAUDEM_PATH/pitremove -z logan.tif -fel loganfel.bil" {
-# cd gridtypes
-# run mpiexec -np 3  $TAUDEM_PATH/pitremove -z logan.tif -fel loganfel.bil
-# assert_success
-# }
 @test "run mpiexec -np 3  $TAUDEM_PATH/d8flowdir -fel loganfel1.bin -p bilp.bil -sd8 binsd8.bin" {
 cd gridtypes
 run mpiexec -np 3  $TAUDEM_PATH/d8flowdir -fel loganfel1.bin -p bilp.bil -sd8 binsd8.bin
@@ -906,7 +905,7 @@ assert_success
 }
 
 # Setregion function which defines a region with the indicated ID if either it is in the gw file or has flow going to it, even though its flow direction may be no data
-@test "mpiexec -n 3 $TAUDEM_PATH/setregion -p fdrsubset.tif -gw gwsubset.tif -out region3.tif -id 3" {
+@test "run mpiexec -n 3 $TAUDEM_PATH/setregion -p fdrsubset.tif -gw gwsubset.tif -out region3.tif -id 3" {
 cd SetRegion
 run mpiexec -n 3  $TAUDEM_PATH/setregion -p fdrsubset.tif -gw gwsubset.tif -out region3.tif -id 3
 assert_success
@@ -1285,7 +1284,7 @@ run mpiexec -np 8  $TAUDEM_PATH/moveoutletstostreams  -p  subwatershed_74p.tif  
 assert_success
 }
 
-@test "run mpiexec -np 8  $TAUDEM_PATH/moveoutletstostreams  -p  subwatershed_74p.tif  -src  subwatershed_74src1.tif  -o  testpoints.shp  -om  New_Outlet2.shp  -md  100" {
+@test "run mpiexec -np 8  $TAUDEM_PATH/moveoutletstostreams  -p p.tif -src src.tif -o outlets2_geo.shp -om outlets_align.shp -md 3000" {
 cd MoveOutlets3
 run mpiexec -np 8  $TAUDEM_PATH/moveoutletstostreams  -p p.tif -src src.tif -o outlets2_geo.shp -om outlets_align.shp -md 3000
 assert_success
@@ -1341,8 +1340,8 @@ assert_success
 }
 
 #Run AreaD8 for a weight file that does not have nodata value set to test GDAL setting of nodata
-@test "run mpiexec -n 4  $TAUDEM_PATH/aread8 aread8 -p p.tif -ad8 ssa.tif -wg weights.tif -nc" {
+@test "run mpiexec -n 4  $TAUDEM_PATH/aread8 -p p.tif -ad8 ssa.tif -wg weights.tif -nc" {
 cd GDAL_unset_nodata
-run mpiexec -n 4  $TAUDEM_PATH/aread8 aread8 -p p.tif -ad8 ssa.tif -wg weights.tif -nc
+run mpiexec -n 4  $TAUDEM_PATH/aread8 -p p.tif -ad8 ssa.tif -wg weights.tif -nc
 assert_success
 }
